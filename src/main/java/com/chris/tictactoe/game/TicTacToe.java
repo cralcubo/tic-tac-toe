a110f1d4829c0622cccc7a3ba49fcd96a349398f
Christian Roman <cralcubo@hotmail.com>
2014-07-17 15:59:24 +0200

    Ignore file added and more documentation added to the TicTacToe interface.

--- src/main/java/com/chris/tictactoe/game/TicTacToe.java
+++ src/main/java/com/chris/tictactoe/game/TicTacToe.java
@@ -28,17 +28,43 @@ public interface TicTacToe {
 	/**
 	 * Method that start the game.
 	 * This method should be called after 2 players are registered
-	 * with the method @link registerPlayers
+	 * with the method {@link #registerPlayers(Player, Player) registerPlayers} 
+	 * 
 	 * @throws NoPlayersRegisteredException
 	 */
 	void startGame() throws NoPlayersRegisteredException;
 	
+	/**
+	 * Place a 'X' in the matrix of the game.
+	 * 
+	 * @param coordinate
+	 * @throws PositionOccupiedException
+	 * @throws WaitYourTurnException
+	 * @throws GameOverException
+	 */
 	void playCross(GameCoordinates coordinate) throws PositionOccupiedException, WaitYourTurnException, GameOverException;
 	
+	/**
+	 * Place a 'O' in the matrix of the game.
+	 * 
+	 * @param coordinate
+	 * @throws PositionOccupiedException
+	 * @throws WaitYourTurnException
+	 * @throws GameOverException
+	 */
 	void playCircle(GameCoordinates coordinate) throws PositionOccupiedException, WaitYourTurnException, GameOverException;
 	
+	/**
+	 * Method that will return the winner player in case there is a winner.
+	 * 
+	 * @return
+	 */
 	Player checkWinner();
 	
+	/**
+	 * Method that will verify if the game is a tie. 
+	 * @return
+	 */
 	boolean isTie();
 	
 	
