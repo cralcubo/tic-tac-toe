ae26ff1c64af7bff1594e161c5320e192863f14a
Christian Roman <cralcubo@hotmail.com>
2014-07-17 15:43:18 +0200

    Documentation added to the game interface

--- src/main/java/com/chris/tictactoe/game/TicTacToe.java
+++ src/main/java/com/chris/tictactoe/game/TicTacToe.java
@@ -6,11 +6,31 @@ import com.chris.tictactoe.game.exceptions.PositionOccupiedException;
 import com.chris.tictactoe.game.exceptions.WaitYourTurnException;
 import com.chris.tictactoe.game.exceptions.WrongShapeException;
 import com.chris.tictactoe.game.model.Player;
-
+/**
+ * Interface that has all the actions
+ * to play the Tic Tac Toe game.
+ * 
+ * @author christian
+ *
+ */
 public interface TicTacToe {
 	
+	/**
+	 * Method that will register two players
+	 * to play the game.
+	 * 
+	 * @param circlePlayer
+	 * @param crossPlayer
+	 * @throws WrongShapeException
+	 */
 	void registerPlayers(Player circlePlayer, Player crossPlayer) throws WrongShapeException;
 	
+	/**
+	 * Method that start the game.
+	 * This method should be called after 2 players are registered
+	 * with the method @link registerPlayers
+	 * @throws NoPlayersRegisteredException
+	 */
 	void startGame() throws NoPlayersRegisteredException;
 	
 	void playCross(GameCoordinates coordinate) throws PositionOccupiedException, WaitYourTurnException, GameOverException;
